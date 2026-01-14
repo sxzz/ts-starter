bea69a43108f1e93ec973fe23a2ca9c105d0f0e1
Kevin Deng <sxzz@sxzz.moe>
2026-01-14 18:56:51 +0800

    feat: add tsdown preset

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,3 @@
-import { defineConfig } from 'tsdown'
+import { lib } from 'tsdown-preset-sxzz'
 
-export default defineConfig({
-  platform: 'neutral',
-  exports: true,
-  inlineOnly: [],
-})
+export default lib()
